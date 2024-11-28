2563f6ba3b27ae4e48f1d146b954506867e7ef0f
Florent Lahalle <flahalle@z4r6p5.42lyon.fr>
2024-11-28 20:45:53 +0100

    begin project printf

---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME 		= 	libftprintf.a
+CC 		= 	cc
+HEAD		=	printf.h
+CFLAGS 		= 	-Wall -Werror -Wextra -g
+SRC 		= 	
+
+
+OBJ 		= 	$(SRC:%.c=%.o)
+
+all: 			$(NAME)
+
+$(NAME): 		$(OBJ)
+				ar rcs $(NAME) $(OBJ)
+
+%.o: %.c		$(HEAD) Makefile
+				$(CC) $(CFLAGS) -c $< -o $@
+clean:
+				rm -f $(OBJ) $(OBJ_BONUS)
+
+fclean:			clean
+				rm -f $(NAME)
+
+re:				fclean all
+
+.PHONY:				all clean fclean re
---
+++ printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flahalle <flahalle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 17:05:06 by flahalle          #+#    #+#             */
+/*   Updated: 2024/11/28 17:13:07 by flahalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <ctype.h>
+# include <limits.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h> 
+
+#endif
